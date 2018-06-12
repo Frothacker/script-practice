86b3d236c0e62ee38b2693e051194fb8f0da6629
Finn <finn.rothacker@gmail.com>
2018-06-12 12:35:00 +0800

    added outputs folder

---
+++ outputs/outputs.js
@@ -0,0 +1,15 @@
+    function writeTest() {
+      document.write( "Yup, write() works");
+    }
+
+    function innerHtmlTest() {
+      document.getElementById('change_me').innerHTML = "a slightly more complex line of text";
+    }
+
+    function alertTest() {
+      window.alert("yup, this is an alert");
+    }
+
+    function consoleLogTest() {
+      console.log('logging to the console');
+    }
