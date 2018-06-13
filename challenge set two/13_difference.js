4a35160b480a0def24ae97453e202f7a00f5e24d
Finn <finn.rothacker@gmail.com>
2018-06-13 13:28:02 +0800

    added difference from 13 and ability to get the filename

---
+++ challenge set two/13_difference.js
@@ -0,0 +1,8 @@
+
+let num = window.prompt('input a number');
+
+if (num < 14) {
+  window.alert("the difference between " + 13 + " and the number you chose, " + num + ", is " + (13 - num));
+} else {
+  window.alert("the difference between " + 13 + " and the number you chose, " + num + ", times two, is " + ((num - 13) * 2));
+}
---
+++ challenge set two/filename_extention.js
@@ -0,0 +1,12 @@
+
+let file1 = 'test.rb';
+let file2 = 'test.xml';
+let file3 = 'test.html';
+let file4 = 'test.json';
+let file5 = 'test.js';
+
+console.log(file1.split('.').pop());
+console.log(file2.split('.').pop());
+console.log(file3.split('.').pop());
+console.log(file4.split('.').pop());
+console.log(file5.split('.').pop());
