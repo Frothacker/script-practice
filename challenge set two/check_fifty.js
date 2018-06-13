6ee170a8475525c7aa7041d161ba6b689ca83eaf
Finn <finn.rothacker@gmail.com>
2018-06-13 13:43:09 +0800

    added fiftey checker

--- challenge set two/check_fifty.js
+++ challenge set two/check_fifty.js
@@ -1,1 +1,10 @@
-let num1 = +(prompt("first number"));
+let num1 = +(prompt("input first number"));
+let num2 = +(prompt("input second number"));
+
+if (num1 === 50 || num2 === 50) {
+   alert('one of these is 50');
+} else if(num1 + num2 === 50) {
+  alert('they add to 50');
+} else {
+  alert('Neither number is 50, nor do they add to 50');
+}
